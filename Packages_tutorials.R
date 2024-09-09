1b9d10f332d0165ee67ac1ab957557e0f571bc90
EdyKayi <edemtitriku1@gmail>
2024-09-09 12:30:25 +0000

    Initial commit

--- Packages_tutorials.R
+++ Packages_tutorials.R
@@ -5,6 +5,13 @@ library(devtools)
 library(roxygen2)
 
 use_git_config(user.name="EdyKayi", user.email="edemtitriku1@gmail")
+
+usethis::create_github_token()
+
+credentials::set_github_pat()
+
+usethis::use_github()
+
 use_git()
 
 usethis::use_r("mysumfunction")
